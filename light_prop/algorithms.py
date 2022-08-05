9a96d7937c42e36c25c00debdc7db23c63127161
Bartosz Brozyna <bbrozyna@gmail.com>
2022-08-05 18:24:25 +0200

    critical changes

--- light_prop/algorithms.py
+++ light_prop/algorithms.py
@@ -2,23 +2,23 @@
 """
 Created on Thu Aug  4 14:09:21 2022
 
-@author: PK
+@author: dr PK
 """
 
-import light_prop.lightfield as lf
 import light_prop.propagation.methods as prop
 from light_prop.propagation.params import PropagationParams
+from light_prop.lightfield import LightField
 
 
 class GerchbergSaxton:
     def __init__(self, propagation_params: PropagationParams):
         self.params = propagation_params
 
-    def optimize(self, input_field: lf.LightField, target: lf.LightField, n: int):
+    def optimize(self, input_field: LightField, target: LightField, iterations: int):
         output_plane = target
         input_plane = None
 
-        for i in range(n):
+        for i in range(iterations):
             output_plane.amplitude = target.amplitude
             self.params.distance *= -1
             conv = prop.ConvolutionPropagation(self.params)
