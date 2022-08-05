85a4f4c562185c18f5b77819a8e254fe837f3b20
Bartosz Brozyna <bbrozyna@gmail.com>
2022-08-05 15:42:10 +0200

    preventing GS example from launching when imported

--- GS_example.py
+++ GS_example.py
@@ -12,31 +12,33 @@ import numpy as np
 from light_prop.algorithms import GerchbergSaxton
 from light_prop.visualisation import GeneratePropagationPlot
 
-params = PropagationParams.get_example_propagation_data()
 
-target_sigma = 2
+if __name__ == "__main__":
+    params = PropagationParams.get_example_propagation_data()
 
-amp = np.array(
-    [[calc.gaussian(np.sqrt(x ** 2 + y ** 2), params.sigma) for x in
-      np.arange(-params.matrix_size / 2, params.matrix_size / 2) * params.pixel] for y in
-     np.arange(-params.matrix_size / 2, params.matrix_size / 2) * params.pixel])
+    target_sigma = 2
 
-target = amp = np.array(
-    [[calc.gaussian(np.sqrt(x ** 2 + y ** 2), target_sigma) for x in
-      np.arange(-params.matrix_size / 2, params.matrix_size / 2) * params.pixel] for y in
-     np.arange(-params.matrix_size / 2, params.matrix_size / 2) * params.pixel])
+    amp = np.array(
+        [[calc.gaussian(np.sqrt(x ** 2 + y ** 2), params.sigma) for x in
+          np.arange(-params.matrix_size / 2, params.matrix_size / 2) * params.pixel] for y in
+         np.arange(-params.matrix_size / 2, params.matrix_size / 2) * params.pixel])
 
-phase = np.array(
-    [[0 for x in
-      np.arange(-params.matrix_size / 2, params.matrix_size / 2) * params.pixel] for y in
-     np.arange(-params.matrix_size / 2, params.matrix_size / 2) * params.pixel])
+    target = amp = np.array(
+        [[calc.gaussian(np.sqrt(x ** 2 + y ** 2), target_sigma) for x in
+          np.arange(-params.matrix_size / 2, params.matrix_size / 2) * params.pixel] for y in
+         np.arange(-params.matrix_size / 2, params.matrix_size / 2) * params.pixel])
 
-GS = GerchbergSaxton(params)
+    phase = np.array(
+        [[0 for x in
+          np.arange(-params.matrix_size / 2, params.matrix_size / 2) * params.pixel] for y in
+         np.arange(-params.matrix_size / 2, params.matrix_size / 2) * params.pixel])
 
-res = GS.optimize(LightField(amp, phase), LightField(target, phase), 5)
+    GS = GerchbergSaxton(params)
 
-plotter = GeneratePropagationPlot(res[1])
-plotter.save_output_as_figure("outs/structure.png", output_type=GeneratePropagationPlot.PLOT_PHASE)
+    res = GS.optimize(LightField(amp, phase), LightField(target, phase), 5)
 
-plotter2 = GeneratePropagationPlot(res[0])
-plotter2.save_output_as_figure("outs/result.png", output_type=GeneratePropagationPlot.PLOT_ABS)
+    plotter = GeneratePropagationPlot(res[1])
+    plotter.save_output_as_figure("outs/structure.png", output_type=GeneratePropagationPlot.PLOT_PHASE)
+
+    plotter2 = GeneratePropagationPlot(res[0])
+    plotter2.save_output_as_figure("outs/result.png", output_type=GeneratePropagationPlot.PLOT_ABS)
