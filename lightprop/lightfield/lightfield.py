89c61c17a4f0f29f9ac638474e0e5458aeabb96a
Michał Doliński <micdolinski@gmail.com>
2022-10-01 18:25:33 +0200

    lightfield refactoring

--- lightprop/lightfield/lightfield.py
+++ lightprop/lightfield/lightfield.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 
-# TODO consider implement slicing (__getitem__)
 class LightField:
     def __init__(self, amp: np.array, phase: np.array):
         if amp.size != phase.size:
@@ -10,15 +9,13 @@ class LightField:
         self.amplitude = amp
         self.phase = phase
 
-    # TODO 'to' suggests rather your own copy of data but its not, maybe just `abs`?
-    def to_abs(self):
+    def abs(self):
         return self.amplitude
 
     def to_intensity(self):
         return self.amplitude ** 2
 
-    # TODO 'to' suggests rather your own copy of data but its not, maybe just `phase`?
-    def to_phase(self):
+    def phase(self):
         return self.phase
 
     def to_re(self):
@@ -43,7 +40,6 @@ class LightField:
         return LightField(amplitude, phase)
 
     def __mul__(self, other):
-        # TODO could be simplified, conversion to complex not necessary to carry out multiplication
         field = self.to_complex() * other.to_complex()
         amplitude = np.abs(field)
         phase = np.angle(field)
