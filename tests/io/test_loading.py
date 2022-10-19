e5846a22e9c043cdd4dd2ade9f04da55529162c6
Michał Doliński <micdolinski@gmail.com>
2022-10-19 23:30:14 +0200

    more tests

--- tests/io/test_loading.py
+++ tests/io/test_loading.py
@@ -20,3 +20,16 @@ def test_should_load_two_complementary_fields(fields, file1, file2):
     field1 = f"{fields[0]}_file"
     field2 = f"{fields[1]}_file"
     assert lp.load(**{field1: file1, field2: file2})
+    assert lp.load(**{field2: file2, field1: file1})
+
+
+@pytest.mark.parametrize("fields", [("re", "amp"), ("re", "phase"), ("im", "amp"), ("im", "phase")])
+@pytest.mark.parametrize("file1", [gray_png, gray_bmp])
+@pytest.mark.parametrize("file2", [gray_png, gray_bmp])
+def test_should_fail_on_non_complementary_fields(fields, file1, file2):
+    field1 = f"{fields[0]}_file"
+    field2 = f"{fields[1]}_file"
+    with pytest.raises(ValueError, match="Invalid arguments provided.*"):
+        lp.load(**{field1: file1, field2: file2})
+    with pytest.raises(ValueError, match="Invalid arguments provided.*"):
+        lp.load(**{field2: file2, field1: file1})
